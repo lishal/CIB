223ecfef828d3481270c749d5532ffe63c1ac0ac
lishal <52682738+lishal@users.noreply.github.com>
2023-07-14 17:08:51 +0530

    Login Page V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { CardModule } from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, CardModule],
+  imports: [BrowserModule, AppRoutingModule, CardModule,ButtonModule,InputTextModule,PasswordModule],
   providers: [],
   bootstrap: [AppComponent],
 })
