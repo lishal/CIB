2f393eebdd236d1fb470c6800aa07d3acebe3618
lishal <lbhari188@gmail.com>
2023-07-14 23:47:13 +0530

    Login page invalid fixes

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string="";
+  password: string="";
+  isCredentialsFailed: boolean=false;
+  onLogin():void{
 
+    if(this.username==="admin" && this.password==="admin"){
+      this.isCredentialsFailed = false;
+      console.log("Login Success")
+    }
+    else if(this.username===""&& this.password===""){
+      this.isCredentialsFailed = true;
+    }
+    else{
+      this.isCredentialsFailed = true;
+      console.log(this.isCredentialsFailed)
+      console.log("Login failed")
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
@@ -10,9 +10,10 @@ import {ButtonModule} from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, CardModule,ButtonModule,InputTextModule,PasswordModule],
+  imports: [BrowserModule, AppRoutingModule, CardModule,ButtonModule,InputTextModule,PasswordModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
