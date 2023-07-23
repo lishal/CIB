89bbc1ac84427931895a8561892fe892d52104ca
lishal <lbhari188@gmail.com>
2023-07-24 01:01:10 +0530

    Left side panel and right side panel created!

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
+  isSidebarSemiClosed = false;
 
+  toggleSidebar() {
+    console.log('I am clicked!');
+    this.isSidebarSemiClosed = !this.isSidebarSemiClosed;
+  }
+  // Right Panel
+  displayRightPanel = false;
+
+  showRightPanel() {
+    this.displayRightPanel = true;
+  }
+
+  hideRightPanel() {
+    this.displayRightPanel = false;
+  }
+  menuItems = [
+    {
+      label: 'Home',
+      icon: 'pi pi-home',
+      routerLink: ['/home'],
+    },
+    {
+      label: 'About',
+      icon: 'pi pi-info',
+      routerLink: ['/about'],
+    },
+    {
+      label: 'Contact',
+      icon: 'pi pi-envelope',
+      routerLink: ['/contact'],
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,25 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { CardModule } from 'primeng/card';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
-
+import { SidebarModule } from 'primeng/sidebar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, CardModule,ButtonModule,InputTextModule,PasswordModule,FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    FormsModule,
+    SidebarModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
