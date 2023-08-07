3dd7eadb8750af7c0ba51cb28f1f7d79e6a30aef
lishal <52682738+lishal@users.noreply.github.com>
2023-08-07 17:22:21 +0530

    Company layout

--- src/app/Components/Setup/company/company.component.ts
+++ src/app/Components/Setup/company/company.component.ts
@@ -8,7 +8,10 @@ import { MenuItem } from 'primeng/api';
 })
 export class CompanyComponent implements OnInit {
   items: MenuItem[] | undefined;
-
+  test:[{
+    label:'Company Setup'
+    icon:'fa-solid fa-building'
+  }] | undefined;
   ngOnInit() {
     this.items = [
       { label: 'Company' },
