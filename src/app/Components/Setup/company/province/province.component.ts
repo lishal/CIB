bc7a23155e4b51033955f7ce96c6893dccd13198
lishal <lbhari188@gmail.com>
2023-08-15 23:13:12 +0530

    Province edit update and deleted added

--- src/app/Components/Setup/company/province/province.component.ts
+++ src/app/Components/Setup/company/province/province.component.ts
@@ -1,28 +1,41 @@
-import { Component,OnInit } from '@angular/core';
-import {ProvinceService} from '../../../../Services/Setup/province.service'
+import { Component, OnInit } from '@angular/core';
+import { ProvinceService } from '../../../../Services/Setup/province.service';
 
 @Component({
   selector: 'app-province',
   templateUrl: './province.component.html',
-  styleUrls: ['./province.component.css']
+  styleUrls: ['./province.component.css'],
 })
 export class ProvinceComponent implements OnInit {
-  data:any[]=[];
-  isLoading:boolean=false;
-  constructor(private api:ProvinceService){}
+  data: any[] = [];
+  isLoading: boolean = false;
+  constructor(private api: ProvinceService) {}
 
-  ngOnInit(): void {
-    this.isLoading=true;
-  
-    this.api.fetchData().subscribe((response)=>{
-      this.data=response;
-      this.isLoading=false;
-      // console.log(this.data)
-    },
-    (error)=>{
-      console.log(error)
-      this.isLoading=false;
-    })
+  showData(id: number) {
+    const displayData = this.data.find((data) => data.id === id);
+    console.log(displayData);
   }
+  editData(id: number) {
+    const editData = this.data.find((data) => data.id === id);
+    console.log(editData);
+  }
+  deleteData(id: number) {
+    const deleteData = this.data.find((data) => data.id === id);
+    console.log(deleteData);
+  }
+  ngOnInit(): void {
+    this.isLoading = true;
 
+    this.api.fetchData().subscribe(
+      (response) => {
+        this.data = response;
+        this.isLoading = false;
+        // console.log(this.data)
+      },
+      (error) => {
+        console.log(error);
+        this.isLoading = false;
+      }
+    );
+  }
 }
--- src/app/Services/Setup/province.service.ts
+++ src/app/Services/Setup/province.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import{HttpClient} from '@angular/common/http'
-import {Observable} from 'rxjs'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProvinceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  fetchData():Observable<any>{
-    const apiUrl='https://random-data-api.com/api/v2/banks/?size=100';
+  fetchData(): Observable<any> {
+    const apiUrl = 'https://random-data-api.com/api/v2/banks/?size=100';
     return this.http.get(apiUrl);
   }
 }
