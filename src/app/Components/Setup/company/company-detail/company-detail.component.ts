11dcc4e7b0e76fa107d0b5541e125769ac9bab11
lishal <lbhari188@gmail.com>
2023-08-17 22:01:23 +0530

    Value added in company details

--- src/app/Components/Setup/company/company-detail/company-detail.component.ts
+++ src/app/Components/Setup/company/company-detail/company-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit,OnDestroy  } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { CompanyDetailService } from '../../../../Services/Setup/Company/company-detail.service';
 import { ViewComponent } from './view/view.component';
@@ -7,32 +7,37 @@ import { EditComponent } from './edit/edit.component';
   selector: 'app-company-detail',
   templateUrl: './company-detail.component.html',
   styleUrls: ['./company-detail.component.css'],
-  providers:[DialogService],
+  providers: [DialogService],
 })
-
 export class CompanyDetailComponent implements OnInit {
   ref: DynamicDialogRef | undefined;
-  constructor(private api: CompanyDetailService,public dialogService: DialogService) {}
-  data=[{
-    id:1,
-    companyName:'NIC Asia Bank Ltd',
-    dpid:'014',
-    prevDpid:0,
-    tradingStm:'',
-    secretKey:''
-  }]
+  constructor(
+    private api: CompanyDetailService,
+    public dialogService: DialogService
+  ) {}
+  data = [
+    {
+      id: 1,
+      companyName: 'NIC Asia Bank Ltd',
+      abbreveration: '',
+      dpid: '014',
+      prevDpid: 0,
+      tradingStm: '',
+      secretKey: '',
+    },
+  ];
 
   showData(id: number) {
     const displayData = this.data.find((data) => data.id === id);
     this.ref = this.dialogService.open(ViewComponent, {
       header: `Detailed View of ${displayData?.id} account number`,
       width: '70%',
-      height:'80%',
+      height: '80%',
       contentStyle: { overflow: 'auto' },
       baseZIndex: 10000,
       maximizable: true,
-      data : displayData
-  });
+      data: displayData,
+    });
   }
 
   editData(id: number) {
@@ -40,16 +45,15 @@ export class CompanyDetailComponent implements OnInit {
     this.ref = this.dialogService.open(EditComponent, {
       header: `Edit Company`,
       width: '70%',
-      height:'80%',
+      height: '80%',
       contentStyle: { overflow: 'auto' },
       baseZIndex: 10000,
       maximizable: true,
-      data : editData
-  });
+      data: editData,
+    });
   }
 
   ngOnInit() {
     this.data;
   }
-
 }
--- src/app/Components/Setup/company/company-detail/edit/edit.component.ts
+++ src/app/Components/Setup/company/company-detail/edit/edit.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { DynamicDialogRef,DialogService,DynamicDialogConfig } from 'primeng/dynamicdialog';
+import {
+  DynamicDialogRef,
+  DialogService,
+  DynamicDialogConfig,
+} from 'primeng/dynamicdialog';
+import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
-import{ButtonModule} from 'primeng/button'
-
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
   providers: [DialogService],
-  imports:[CardModule,ButtonModule],
-  standalone:true
+  imports: [CardModule, ButtonModule, CommonModule],
+  standalone: true,
 })
 export class EditComponent implements OnInit {
-  constructor(private dialogService: DynamicDialogConfig, public ref: DynamicDialogRef) {}
+  constructor(
+    private dialogService: DynamicDialogConfig,
+    public ref: DynamicDialogRef
+  ) {}
   data: any[] = [];
-  uploadedImg:string |null =null;
-  handleImage(event:any){
-    const file=event.target.files[0];
-    if(file){
-      const imgData=new FormData();
-      imgData.append('image',file);
+  selectedImageUrl: string | ArrayBuffer | null = null;
+  handleImage(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedImageUrl = e.target.result;
+      };
+      reader.readAsDataURL(file);
     }
   }
   ngOnInit() {
-      this.data=this.dialogService.data;
-      // console.log(this.data);
+    this.data = [this.dialogService.data];
   }
-
   // selectProduct(product: Product) {
   //     this.ref.close(product);
   // }
